f26c926a88a6502133e512a71a043c476f644008
chaudharynitin <nitins818@gmail.com>
2017-04-27 12:58:35 +0530

    Update workfromhome.pl

--- workfromhome.pl
+++ workfromhome.pl
@@ -10,10 +10,11 @@ my $day = substr($date,0,3);
 
 if($day ne 'Sat' || $day ne 'Sun') #Skip on weekends
 {
-        my $subjectDate = substr($date,0,10);
-        my $emailSubject = "Working From Home ( $subjectDate )";
         my $reason = ['Not Well','Suffering From Fever','Going to doctor','Worked late last night','Missed  CAB'];
         my $rand = int(rand(5));
+        my $subjectDate = substr($date,0,10);
+        my $emailSubject = "Working From Home ( $subjectDate )";
+        my $emailBody = "Dear Sir,\n\nI am working from home today \n Reason : $reason->[$rand]\n \n --\nRegards,\nNitin Chaudhary";        
 
-        `echo -e "Dear Sir,\n\nI am working from home today \n Reason : $reason->[$rand]\n \n --\nRegards,\nNitin Chaudhary" | mail -s "$emailSubject" nitinc`;
+        `echo -e "$emailBody" | mail -s "$emailSubject" nitinc`;
 }
