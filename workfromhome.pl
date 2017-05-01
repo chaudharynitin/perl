b86d3c018f410f8d782190bda0c80690de86b8ce
chaudharynitin <nitins818@gmail.com>
2017-05-01 12:05:01 +0530

    Update workfromhome.pl

--- workfromhome.pl
+++ workfromhome.pl
@@ -5,16 +5,12 @@ use warnings;
 
 my $date = `date`;
 
-#To find out current day
 my $day = substr($date,0,3);
-
-if($day ne 'Sat' || $day ne 'Sun') #Skip on weekends
+if($day ne 'Sun' and $day ne 'Sat')
 {
-        my $reason = ['Not Well','Suffering From Fever','Going to doctor','Worked late last night','Missed  CAB'];
-        my $rand = int(rand(5));
         my $subjectDate = substr($date,0,10);
         my $emailSubject = "Working From Home ( $subjectDate )";
-        my $emailBody = "Dear Sir,\n\nI am working from home today \n Reason : $reason->[$rand]\n \n --\nRegards,\nNitin Chaudhary";        
-
-        `echo -e "$emailBody" | mail -s "$emailSubject" nitinc`;
+        my $reason = ['Not Well','Suffering From Fever','Going to doctor','Worked late last night','Missed  CAB'];
+        my $rand = int(rand(5));
+        `echo -e "Dear Sir,\n\nI am working from home today \n Reason : $reason->[$rand]\n \n --\nRegards,\nNitin Chaudhary" | mail -s "$emailSubject" nitinc`;
 }
