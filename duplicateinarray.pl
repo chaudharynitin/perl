f8a750a642258ecf58e260d99fa6c7f177c5ed55
chaudharynitin <nitins818@gmail.com>
2017-05-01 15:37:06 +0530

    Create duplicateinarray.pl
    find duplicate number in array

---
+++ duplicateinarray.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+my @numbers = (1,2,3,1,2,3,4,5,11,6,7,8,93,3,3,3);
+my @duplicate = ();
+my %temp = ();
+foreach my $i (@numbers)
+{
+        $temp{$i}++;
+}
+
+foreach my $key (keys %temp)
+{
+        if($temp{$key} == 1)
+        {
+                push (@duplicate,$key);
+        }
+}
+print "@duplicate";
